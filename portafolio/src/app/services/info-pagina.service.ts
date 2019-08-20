b8537f6b5b334c806b3fec48c2e01abb4f7fa51e
Luis García <luis_grdz@hotmail.com>
2019-08-20 00:41:41 -0500

    video 33: después de crear la base de datos en firebas, carga la información para ser utilizada en la página

--- portafolio/src/app/services/info-pagina.service.ts
+++ portafolio/src/app/services/info-pagina.service.ts
@@ -9,9 +9,17 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+
+  equipo: any[] = [];
+
   constructor( private http: HttpClient) {
     // console.log('Servicio de infoPagina listo')
+    this.cargar_info();
 
+    this.cargar_equipo();
+  }
+
+  private cargar_info() {
     // Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json').subscribe( (resp: InfoPagina) => {
 
@@ -19,7 +27,19 @@ export class InfoPaginaService {
       this.info = resp;
 
       // console.log(resp['twitter']);
+      // console.log(resp);
+    });
+
+   }
+
+   private cargar_equipo() {
+
+    this.http.get('https://angular-webapp-b95ee.firebaseio.com/equipo.json')
+      .subscribe( (resp: any[]) => {
+
+      this.equipo = resp;
       console.log(resp);
     });
+
    }
 }
