378397fee39ed4f41adc237f4b1b92cab9248b13
Luis García <luis_grdz@hotmail.com>
2019-08-31 10:20:42 -0500

    Fix: se elimina una linea que no estaba siendo utilizada

--- portafolio/src/app/services/productos.service.ts
+++ portafolio/src/app/services/productos.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../interfaces/producto.interface';
-import { promise } from 'protractor';
 
 @Injectable({
   providedIn: 'root'
