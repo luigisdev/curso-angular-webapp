d4229290a0467c685c2f2a341fabc7d903f5dfa0
Luis García <luis_grdz@hotmail.com>
2019-08-31 10:14:07 -0500

    Video 50: logica del proceso de carga y filtro de las búsquedas

--- portafolio/src/app/services/productos.service.ts
+++ portafolio/src/app/services/productos.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../interfaces/producto.interface';
+import { promise } from 'protractor';
 
 @Injectable({
   providedIn: 'root'
@@ -17,15 +18,24 @@ export class ProductosService {
   }
 
   private cargarProductos() {
-    this.http.get('https://angular-webapp-b95ee.firebaseio.com/productos_idx.json')
-    .subscribe( ( resp: Producto[] ) => {
-      console.log(resp);
-      this.productos = resp;
-      // this.cargando = false;
-      setTimeout( () => {
-        this.cargando = false;
-      }, 1500);
+
+    // Utilización de promesas
+
+    return new Promise( (resolve, reject) => {
+
+      this.http.get('https://angular-webapp-b95ee.firebaseio.com/productos_idx.json')
+        .subscribe( ( resp: Producto[] ) => {
+          // console.log(resp);
+          this.productos = resp;
+          // this.cargando = false;
+          setTimeout( () => {
+            this.cargando = false;
+          }, 1500);
+          resolve();
+      });
+
     });
+
   }
 
   getProducto(id: string) {
@@ -33,10 +43,42 @@ export class ProductosService {
   }
 
   buscarProducto( termino: string ) {
-    this.productosFiltrado = this.productos.filter( producto => {
-      return true;
-    });
+
+    if ( this.productos.length === 0 ) {
+      // cargar productos
+      this.cargarProductos().then( ()=> {
+        // ejecutar después de tener los productos cargados
+        // aplcar filtro
+        this.filtrarProductos( termino );
+      });
+    } else {
+      // aplicar el filtro
+      this.filtrarProductos( termino );
+    }
 
     console.log( this.productosFiltrado );
   }
+
+  filtrarProductos( termino: string ) {
+
+    // console.log( this.productos );
+    // se resetea el arreglo de los productos filtrados cada vez que se hace una nueva busqueda
+    this.productosFiltrado = [];
+
+    // se convierte a minusculas el termino de búsquedas
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach( prod => {
+
+      // ya que la categoria siempre es minuscula no se webkitConvertPointFromNodeToPage, pero el título si
+      // para que no tome en cuenta las mayusculas introducidas
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+
+      if ( prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0 ) {
+        this.productosFiltrado.push( prod );
+      }
+    });
+
+  }
+
 }
